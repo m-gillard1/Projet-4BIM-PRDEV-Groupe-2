727a23c0ccf20f10bea12f069d3a0f00363871a5
TheoDucasse <tducasse0@gmail.com>
2024-02-23 13:56:05 +0100

    separation de la fenetre principale en deux frames

--- interface.py
+++ interface.py
@@ -13,6 +13,18 @@ root.title("Face Determination Software")
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
+# Calculate the width for the left and right frames
+left_width = screen_width * 3 // 7
+right_width = screen_width * 4 // 7
+
+
+# Create frames for left and right sections
+left_frame = tk.Frame(root, width=left_width, height=screen_height, bg="gray80")
+right_frame = tk.Frame(root, width=right_width, height=screen_height, bg="gray50")
+
+# Pack the frames to the left and right respectively
+left_frame.pack(side=tk.LEFT, fill=tk.Y)
+right_frame.pack(side=tk.RIGHT, fill=tk.Y)
 
 
 
