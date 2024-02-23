145ef1603cf008569db9d4ccba0669f151b57591
TheoDucasse <tducasse0@gmail.com>
2024-02-23 14:00:12 +0100

    modification du frame de droite pour le diviser en deux nouveau frames, un pour contenir les images et un pour des boutons pour l'utilisateur

--- interface.py
+++ interface.py
@@ -16,16 +16,23 @@ screen_height = root.winfo_screenheight()
 # Calculate the width for the left and right frames
 left_width = screen_width * 3 // 7
 right_width = screen_width * 4 // 7
-
+choices_height = screen_height * 2 // 3
 
 # Create frames for left and right sections
 left_frame = tk.Frame(root, width=left_width, height=screen_height, bg="gray80")
 right_frame = tk.Frame(root, width=right_width, height=screen_height, bg="gray50")
+choices_frame = tk.Frame(right_frame,width=right_width, height = choices_height, bg = "gray70")
+container_frame =  tk.Frame(choices_frame,width=right_width*0.9, height = choices_height*0.9, bg = "white")
 
 # Pack the frames to the left and right respectively
 left_frame.pack(side=tk.LEFT, fill=tk.Y)
 right_frame.pack(side=tk.RIGHT, fill=tk.Y)
+choices_frame.pack(side=tk.TOP, fill=tk.X)
 
+
+# Create a frame to contain the grid of frames
+container_frame.pack(fill="both", expand=True)
+container_frame.place(relx=0.5,rely=0.5,anchor="center")
 
 
         
