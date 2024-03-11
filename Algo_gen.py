a70db91236d1e61dadcc906e1835f7406cd2f8f4
Selma Kadiri <selma.kadiri@insa-lyon.fr>
2024-03-11 09:29:46 +0100

    Ajout de la fonction lowest_cost_pop(sorted_image_list)

--- Algo_gen.py
+++ Algo_gen.py
@@ -29,3 +29,24 @@ def pop_sort(image_list):
     sorted_image_list = sorted(image_list, key=lambda x: x[0][0], reverse=True)
 
     return sorted_image_list
+
+def lowest_cost_pop(sorted_image_list):
+    """
+    Retourne la première moitié de vecteurs (ceux ayant le plus faible cout = la meilleure note).
+
+    Paramètres :
+    ----------
+
+    sorted_image_list: list
+        image_list ordonnées par ordre decroissant selon les notes (couts)
+
+    Retourne :
+    ---------
+    low_pop : list
+        Listes contenant les images + notes ayant le plus faible cout.
+
+    """
+    nombre_image=len(sorted_image_list)
+    Ns = nombre_image // 2
+    low_pop = sorted_image_list[0:Ns]
+    return low_pop
