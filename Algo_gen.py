4614343df55a07cd4c6c08089afd3a34889f54a8
Selma Kadiri <selma.kadiri@insa-lyon.fr>
2024-03-11 09:28:33 +0100

    Ajout de la fonction pop_sort(image_list)

--- Algo_gen.py
+++ Algo_gen.py
@@ -4,3 +4,28 @@ import math
 import numpy as np
 import statistics
 import os
+
+def pop_sort(image_list):
+
+    """
+    Trie les vecteurs en fonction de leur cout de la meilleure à la moins bonne note.
+
+    Paramètres :
+    ----------
+    image_list: list
+        list de vecteur comprenant la note de l'image et le vecteur issue de celle-ci
+
+    Retourne :
+    ---------
+    sorted_image_list: image_list ordonnées par ordre decroissant selon les notes (couts)
+
+    """
+
+    sorted_image_list = []
+
+
+
+    # Trier la liste de listes selon le deuxième élément de chaque sous-liste
+    sorted_image_list = sorted(image_list, key=lambda x: x[0][0], reverse=True)
+
+    return sorted_image_list
