2b9c6aa8be28dd1c86065abd5c10886e487ea299
tchalas1 <159763666+tchalas1@users.noreply.github.com>
2024-04-02 15:32:47 +0200

    creation of the class VAE

--- Variational_Autoencoder_Training.py
+++ Variational_Autoencoder_Training.py
@@ -1,3 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+
+class VAE(nn.Module):
+    def __init__(self, latent_dim):
+        super(VAE, self).__init__()
+        self.latent_dim = latent_dim
