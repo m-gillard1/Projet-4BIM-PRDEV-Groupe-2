f5fccac65879c05dc0f10da91a9b3491a221dc2c
tchalas1 <159763666+tchalas1@users.noreply.github.com>
2024-04-02 15:34:25 +0200

    creation of the mean and logvar argument for the variational part

--- Variational_Autoencoder_Training.py
+++ Variational_Autoencoder_Training.py
@@ -18,3 +18,7 @@ class VAE(nn.Module):
             nn.Conv2d(64,128, kernel_size=3, stride=2, padding=1),
             nn.ReLU()
         )
+
+        # Mean and log variance layers
+        self.fc_mean = nn.Linear(128 * 32 * 32, latent_dim)
+        self.fc_log_var = nn.Linear(128 * 32 * 32 , latent_dim)
