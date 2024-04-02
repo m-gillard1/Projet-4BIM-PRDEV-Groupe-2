af65b7cf831bb2123638712cfda2938a05d95b9a
tchalas1 <159763666+tchalas1@users.noreply.github.com>
2024-04-02 15:33:37 +0200

    creation of the encoded layer

--- Variational_Autoencoder_Training.py
+++ Variational_Autoencoder_Training.py
@@ -6,3 +6,15 @@ class VAE(nn.Module):
     def __init__(self, latent_dim):
         super(VAE, self).__init__()
         self.latent_dim = latent_dim
+
+        # Encoder layers
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(16,32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32,64, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64,128, kernel_size=3, stride=2, padding=1),
+            nn.ReLU()
+        )
