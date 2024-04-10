fa867098d98483fbd833d22d62dec617c5fb1e2d
tchalas1 <159763666+tchalas1@users.noreply.github.com>
2024-04-10 11:53:57 +0200

    Abaissement llearning rate, batch size et sauvegarde du modele a chaque epoch

--- Variational_Autoencoder_Training.py
+++ Variational_Autoencoder_Training.py
@@ -64,7 +64,7 @@ class VAE(nn.Module):
 def my_loader_function(path):
         return Image.open(open(path, 'r+b'))
 
-def train_vae(dataset_path, batch_size=64, learning_rate=0.001, num_epochs=100):
+def train_vae(dataset_path, batch_size=32, learning_rate=0.0001, num_epochs=100):
     
     # Load dataset
     train_dataset= datasets.ImageFolder(root=dataset_path, transform=transforms.ToTensor(), loader=my_loader_function)
@@ -98,6 +98,7 @@ def train_vae(dataset_path, batch_size=64, learning_rate=0.001, num_epochs=100):
     for epoch in range(num_epochs):
         for i, (images, _) in enumerate(train_loader):
             images = Variable(images)
+            images = images.to(device)
             recon_images, mu, log_var = vae(images)
             loss = vae_loss(recon_images, images, mu, log_var)
 
@@ -107,9 +108,8 @@ def train_vae(dataset_path, batch_size=64, learning_rate=0.001, num_epochs=100):
 
             if (i+1) % 2 == 0:
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, len(train_loader), loss.item()))
-        if epoch % 10 == 0:
         # Save the trained VAE model
-            torch.save(vae.state_dict(), 'C:/Users/thiba/Documents/INSA_2023-2024/S2/Software_dev/VAE_Models/vae_model'+str(epoch)+'.pth')
+        torch.save(vae.state_dict(), './vae_model'+str(epoch)+'.pth')
 
 if __name__ == "__main__":
     train_vae(dataset_path="C:/Users/thiba/Documents/INSA_2023-2024/S2/Software_dev/ImagesClassées/train_data_0_23999/")
