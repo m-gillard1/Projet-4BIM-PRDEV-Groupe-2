e1b872860f4658562321a22a0f9840cec5596956
tchalas1 <159763666+tchalas1@users.noreply.github.com>
2024-04-10 11:50:47 +0200

    Changement du running device to cuda

--- Variational_Autoencoder_Training.py
+++ Variational_Autoencoder_Training.py
@@ -73,6 +73,11 @@ def train_vae(dataset_path, batch_size=64, learning_rate=0.001, num_epochs=100):
     # Define VAE model
     vae = VAE()
 
+    #define the running device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print("Training on:", device)
+    vae.to(device)
+    
     # Loss function
     def vae_loss(recon_x, x, mu, log_var, epoch):
         if epoch > 2:
