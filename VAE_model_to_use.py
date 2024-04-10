8a8e4b140a3608ac5a002bce530ef82f5cdaf9fd
tchalas1 <159763666+tchalas1@users.noreply.github.com>
2024-04-10 12:08:45 +0200

    Initialisation du vae

--- VAE_model_to_use.py
+++ VAE_model_to_use.py
@@ -3,3 +3,12 @@ from torchvision import transforms
 from PIL import Image
 import numpy as np
 import variational_Autoencoder_3 as auto
+
+# Instantiate the autoencoder model
+autoencoder = auto.VAE()
+
+# Load the trained weights
+autoencoder.load_state_dict(torch.load("./vae_model10lrBBatchB.pth"))
+
+# Set the model to evaluation mode
+autoencoder.eval()
