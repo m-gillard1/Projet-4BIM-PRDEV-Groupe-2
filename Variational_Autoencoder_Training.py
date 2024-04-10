ad7b1390f68b6202050cd118d51cf74c308a622d
tchalas1 <159763666+tchalas1@users.noreply.github.com>
2024-04-10 12:42:14 +0200

    Ajout docstrings

--- Variational_Autoencoder_Training.py
+++ Variational_Autoencoder_Training.py
@@ -8,6 +8,26 @@ from PIL import Image
 
 
 class VAE(nn.Module):
+    """
+    Variational Autoencoder (VAE) neural network model.
+
+    This VAE model consists of an encoder and a decoder. The encoder takes input images
+    and generates the mean and log variance of the latent space distribution. The decoder
+    reconstructs the input images from the latent space representation.
+
+    Attributes:
+        encoder (nn.Sequential): Encoder layers for feature extraction.
+        fc_mean (nn.Conv2d): Convolutional layer to compute the mean of the latent space.
+        fc_log_var (nn.Conv2d): Convolutional layer to compute the log variance of the latent space.
+        decoder (nn.Sequential): Decoder layers for image reconstruction.
+
+    Methods:
+        encode(self, x): Encodes input images to compute mean and log variance of the latent space.
+        reparameterize(self, mean, log_var): Reparameterizes the latent space.
+        decode(self, z): Decodes the latent space representation to reconstruct input images.
+        forward(self, x): Performs the forward pass through the VAE model.
+    """
+    
     def __init__(self):
         super(VAE, self).__init__()
 
@@ -39,21 +59,61 @@ class VAE(nn.Module):
         )
 
         def encode(self, x):
+            """
+            Encodes input images to compute mean and log variance of the latent space.
+
+            Args:
+                x (torch.Tensor): Input images.
+
+            Returns:
+                torch.Tensor: Mean of the latent space.
+                torch.Tensor: Log variance of the latent space.
+            """
             x = self.encoder(x)
             mean = self.fc_mean(x)
             log_var = self.fc_log_var(x)
             return mean, log_var
 
         def reparameterize(self, mean, log_var):
+            """
+            Reparameterizes the latent space.
+    
+            Args:
+                mean (torch.Tensor): Mean of the latent space.
+                log_var (torch.Tensor): Log variance of the latent space.
+    
+            Returns:
+                torch.Tensor: Reparameterized latent space.
+            """
             std = torch.exp(0.5 * log_var)
             eps = torch.randn_like(std)
             return mean + eps * std
 
         def decode(self, z):
+            """
+            Decodes the latent space representation to reconstruct input images.
+    
+            Args:
+                z (torch.Tensor): Latent space representation.
+    
+            Returns:
+                torch.Tensor: Reconstructed images.
+            """
             x = self.decoder(z)
             return x
 
         def forward(self, x):
+            """
+            Performs the forward pass through the VAE model.
+    
+            Args:
+                x (torch.Tensor): Input images.
+    
+            Returns:
+                torch.Tensor: Reconstructed images.
+                torch.Tensor: Mean of the latent space.
+                torch.Tensor: Log variance of the latent space.
+            """
             x = self.encoder(x)
             mean = self.fc_mean(x)
             log_var = self.fc_log_var(x)
@@ -62,10 +122,28 @@ class VAE(nn.Module):
             return x_recon, mean, log_var
 
 def my_loader_function(path):
+    """
+    Custom loader function to open an image file from the given path.
+
+    Args:
+        path (str): The path to the image file.
+
+    Returns:
+        PIL.Image.Image: The opened image object.
+    """
         return Image.open(open(path, 'r+b'))
 
 def train_vae(dataset_path, batch_size=32, learning_rate=0.0001, num_epochs=100):
-    
+    """
+    Train a Variational Autoencoder (VAE) model.
+
+    Args:
+        dataset_path (str): Path to the directory containing the dataset.
+        batch_size (int, optional): Batch size for training. Defaults to 32.
+        learning_rate (float, optional): Learning rate for optimization. Defaults to 0.0001.
+        num_epochs (int, optional): Number of epochs for training. Defaults to 100.
+    """
+
     # Load dataset
     train_dataset= datasets.ImageFolder(root=dataset_path, transform=transforms.ToTensor(), loader=my_loader_function)
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
