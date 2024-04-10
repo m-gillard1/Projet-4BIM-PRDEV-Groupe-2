63d686da9140722a8cc1595b7b22bcb3073813c6
tchalas1 <159763666+tchalas1@users.noreply.github.com>
2024-04-10 11:38:54 +0200

    Changement strcture VAE
    Retour sur la structre de base de la decode layer, abandon de la couche lineaire (trop couteuse a l'execution)

--- Variational_Autoencoder_Training.py
+++ Variational_Autoencoder_Training.py
@@ -36,16 +36,14 @@ class VAE(nn.Module):
         self.fc_log_var = nn.Linear(128 * 32 * 32 , latent_dim)
 
         self.decoder = nn.Sequential(
-            nn.Linear(latent_dim, 128 * 32 * 32 ),
-            nn.ReLU()
+            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
             nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
             nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.ReLU(),
-            nn.ConvTranspose2d(16, 1, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.Sigmoid()
+            nn.ConvTranspose2d(32, 1, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU()
         )
 
         def encode(self, x):
