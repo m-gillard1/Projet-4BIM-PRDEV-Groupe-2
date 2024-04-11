c734f93e1962a7e62578589f43fbd84d51871564
AuroreLH <aurorelh@outlook.fr>
2024-04-11 16:49:55 +0200

    ajout de la documentation

---
+++ doc/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Projet informatique 4 BIM du groupe 2'
+copyright = '2024, S.KADIRI , T.CHALAS , A.LE HOUSSEL , M.GILLARD , T.DUCASSE'
+author = 'S.KADIRI , T.CHALAS , A.LE HOUSSEL , M.GILLARD , T.DUCASSE'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'Français'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
