5c2e3eae48c3ee8a40f5b05b122d2fd511607d57
AuroreLH <aurorelh@outlook.fr>
2024-04-11 10:48:24 +0200

    suppression cross over sans note

--- Algo_gen.py
+++ Algo_gen.py
@@ -5,7 +5,7 @@ import numpy as np
 import statistics
 import os
 
-## n'est pas utile dans la version sans note
+
 def pop_sort(image_list):
 
     """
@@ -30,7 +30,6 @@ def pop_sort(image_list):
     sorted_image_list = sorted(image_list, key=lambda x: x[0][0], reverse=True)
 
     return sorted_image_list
-
 
 
 
@@ -70,40 +69,6 @@ def cross_over_avec_note(best_image_list, Tc):
             #print(tmp)
             new_P[i][1][posc:len(new_P[i][1])] = new_P[indc][1][posc:len(new_P[i][1])]
             new_P[indc][1][posc:len(new_P[i][1])] = tmp
-
-    return new_P
-
-def cross_over_sans_note(best_image_list, Tc):
-    """
-    Fait des croisements entre vecteurs d'image.
-
-    Paramètres :
-    ----------
-    best_image_list: list
-        list de vecteurs comprenant le vecteur issue de l'image pour les meilleures images
-
-    Tc: float
-        Taux de croisement.
-
-    Retourne :
-    ---------
-    new_P : list
-        Liste contenant les vecteurs d'images (sans note) ayant ou non subis des croisement
-    """
-    new_P = np.copy(best_image_list)
-
-    for i in range(0, len(new_P)):
-        if random() < Tc:
-            #print('cross over !! ',i)
-            indc = randint(0, len(new_P) - 1) #choisi l'image avec laquelle il va échanger entre 0 et 10
-            posc = randint(0, len(new_P[i]) - 1) # choisi a quelle position du vecteur on coupe entre 0 et 20
-            #print(indc)
-            #print(posc)
-            #print(len(new_P[i][1]))
-            tmp = new_P[i][posc:len(new_P[i])]
-            #print(tmp)
-            new_P[i][posc:len(new_P[i])] = new_P[indc][posc:len(new_P[i])]
-            new_P[indc][posc:len(new_P[i])] = tmp
 
     return new_P
 
