136055a20fbb3e04189ca6dc4e5abb895c8159f6
tchalas1 <159763666+tchalas1@users.noreply.github.com>
2024-04-12 10:43:28 +0200

    Update run.py
    ajout des imports nécéssaires au déploiement

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
-import main_sprint1
-import Algo_gen
-import Autoencoder_to_use
-import Training_autoencoder
+import subprocess
 
 def run()
+  commande = "pip install -r requirements.txt"
+  subprocess.run(commande, shell=True)
+  import main_sprint1
+  import Algo_gen
+  import Autoencoder_to_use
+  import Training_autoencoder
   import interface
